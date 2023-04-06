cfe6a0e206fbe3aac3769a7295b4c85b3acbe71e
wangyingbin <yingbin.wang@superstar-auto.com>
2023-04-06 14:14:16 +0800

    增加数据库操作

--- WebApi.Core.IRepository/Base/IBaseRepository.cs
+++ WebApi.Core.IRepository/Base/IBaseRepository.cs
@@ -1,15 +1,82 @@
-﻿namespace WebApi.Core.IRepository.Base
+﻿using SqlSugar;
+
+namespace WebApi.Core.IRepository.Base
 {
     public interface IBaseRepository<TEntity> where TEntity : class
     {
-        Task<TEntity> QueryById(object id);
-
-        Task<List<TEntity>> Query();
-
+        #region Create
+        /// <summary>
+        /// 返回是否插入成功
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         Task<bool> Create(TEntity entity);
 
+        /// <summary>
+        /// 返回插入后的完整实体
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<TEntity> CreateNew(TEntity entity);
+
+        /// <summary>
+        /// 批量插入，返回受影响的行数
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <returns></returns>
+        Task<int> Create(List<TEntity> entities);
+        #endregion
+
+        #region Query
+        /// <summary>
+        /// 通过ID查询
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<TEntity> QueryById(object id);
+
+        /// <summary>
+        /// 查询全部
+        /// </summary>
+        /// <returns></returns>
+        Task<List<TEntity>> Query();
+
+        /// <summary>
+        /// 原生SQL查询
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        Task<List<TEntity>> QuerySql(string sql, SugarParameter[] parameters = null);
+
+        #endregion
+
+        #region Update
         Task<bool> Update(TEntity entity);
 
+        #endregion
+
+        #region Delete
         Task<bool> DeleteByIds(object[] ids);
+
+        #endregion
+
+        #region NoGroup
+        /// <summary>
+        /// 同时实体从A转移到B
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="entity"></param>
+        /// <param name="tableName"></param>
+        /// <returns></returns>
+        Task<int> InsertA2B<TEntity>(List<TEntity> entity, string tableName) where TEntity : class, new();
+
+        /// <summary>
+        /// 执行增删改SQL语句
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        Task<int> ExecuteSql(string sql);
+        #endregion
     }
 }
--- WebApi.Core.Repository/Base/BaseRepository.cs
+++ WebApi.Core.Repository/Base/BaseRepository.cs
@@ -1,22 +1,35 @@
-﻿using WebApi.Core.IRepository.Base;
+﻿using SqlSugar;
+using WebApi.Core.IRepository.Base;
 using WebApi.Core.Repository.SqlSugar;
 
 namespace WebApi.Core.Repository.Base
 {
     public class BaseRepository<TEntity> : DbContext<TEntity>, IBaseRepository<TEntity> where TEntity : class, new()
     {
+        #region Create
+        /// <summary>
+        /// 返回是否插入成功
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         public async Task<bool> Create(TEntity entity)
         {
             var res = await Db.Insertable(entity).ExecuteCommandAsync();
             return res > 0;
         }
 
-        public async Task<bool> DeleteByIds(object[] ids)
+        public async Task<TEntity> CreateNew(TEntity entity)
         {
-            var res = await Db.Deleteable<TEntity>().In(ids).ExecuteCommandAsync();
-            return res > 0;
+            return await Db.Insertable(entity).ExecuteReturnEntityAsync();
         }
 
+        public async Task<int> Create(List<TEntity> entities)
+        {
+            return await Db.Insertable(entities).ExecuteCommandAsync();
+        }
+        #endregion
+
+        #region Query
         public async Task<List<TEntity>> Query()
         {
             var res = await Db.Queryable<TEntity>().ToListAsync();
@@ -28,10 +41,59 @@ namespace WebApi.Core.Repository.Base
             return await Db.Queryable<TEntity>().InSingleAsync(id);
         }
 
+        /// <summary>
+        /// 执行Sql语句查询
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public async Task<List<TEntity>> QuerySql(string sql, SugarParameter[] parameters = null)
+        {
+            return await Db.Ado.SqlQueryAsync<TEntity>(sql, parameters);
+        }
+        #endregion
+
+        #region Update
         public async Task<bool> Update(TEntity entity)
         {
             var res = await Db.Updateable(entity).ExecuteCommandAsync();
             return res > 0;
         }
+        #endregion
+
+        #region Delete
+        public async Task<bool> DeleteByIds(object[] ids)
+        {
+            var res = await Db.Deleteable<TEntity>().In(ids).ExecuteCommandAsync();
+            return res > 0;
+        }
+        #endregion
+
+        #region NoGroup
+        /// <summary>
+        /// 执行增删改SQL语句
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public async Task<int> ExecuteSql(string sql)
+        {
+            return await Db.Ado.ExecuteCommandAsync(sql);
+        }
+
+        /// <summary>
+        /// 将实体从A表转移到B表
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity"></param>
+        /// <param name="tableName"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<int> InsertA2B<T>(List<T> entity, string tableName) where T : class, new()
+        {
+            var insert = Db.Insertable(entity).AS(tableName);
+
+            return await insert.ExecuteCommandAsync();
+        }
+        #endregion
     }
 }
